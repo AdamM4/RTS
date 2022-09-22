dae1186b490422b427ac53021027ffa4a116feca
AdamM4 <adamowitkowo@gmail.com>
2022-09-22 11:05:50 +0200

    zmiana2

---
+++ rts.sql
@@ -0,0 +1,18 @@
+INSERT INTO players (name,email,password) Values ("Adam","Adam@gmail.com","adam123");
+INSERT INTO players (name,email,password) Values ("Mateusz","Mateusz@gmail.com","Mateusz123"); 
+INSERT INTO players (name,email,password) Values ("Rafał","Rafał@gmail.com","Rafał123"); 
+INSERT INTO players (name,email,password) Values ("Cyprian","Cyprian@gmail.com","Cyprian123"); 
+INSERT INTO resorces (name,description) Values ("drewno","pali sie"); 
+INSERT INTO resorces (name,description) Values ("glina","klei sie"); 
+INSERT INTO resorces (name,description) Values ("żelazo","jest ciężkie"); 
+INSERT INTO troops (name,description,damage,distance_damage,defense) Values ("Łucznik","atakuje na dystans","5","20","4"); 
+INSERT INTO troops (name,description,damage,distance_damage,defense) Values ("rycerz","atakuje na blisko","25","0","25");
+INSERT INTO troops (name,description,damage,distance_damage,defense) Values ("miecznik","atakuje swoim mieczem","28","0","15");
+INSERT INTO troops (name,description,damage,distance_damage,defense) Values ("kusznik","atakuje na dystans","5","30","6"); 
+INSERT INTO buildings (name) Values ("dół z gliną"); 
+INSERT INTO buildings (name) Values ("huta żelaza"); 
+INSERT INTO buildings (name) Values ("hata drwala");
+INSERT INTO villages (name,y,x) Values ("Wiocha","32","12");
+INSERT INTO villages (name,y,x) Values ("Małachowo","83","92"); 
+INSERT INTO villages (name,y,x) Values ("Gniezno","42","43");
+INSERT INTO villages (name,y,x) Values ("Kołaczkowo","54","20");
